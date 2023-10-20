316253d55bd2e586f618ea18f24c8f6ff4289e5a
Jayesh Bhole <jayeshbhole123@gmail.com>
2023-10-20 19:26:39 +0530

    feat: add intents sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { useAccount, useConnect, useDisconnect, useNetwork } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
+import { Intents } from "@bytekode/intents";
+import { useState } from "react";
+import { zeroAddress } from "viem";
+
+const intents = new Intents("test-api-key");
+
+const handleExecute = async ({
+  chainId,
+  command,
+  signerAddress,
+}: {
+  chainId: string;
+  command: string;
+  signerAddress: string;
+}) => {
+  const txObject = await intents.getTransaction(chainId, command, signerAddress);
+
+  console.log(txObject);
+};
 
 function App() {
+  const [command, setCommand] = useState("");
+  const { address } = useAccount();
+  const { chain } = useNetwork();
+
   return (
     <div className="flex gap-4 justify-center relative items-center w-full h-full flex-col">
       <div className="absolute top-8 right-8 flex gap-4">
@@ -11,9 +34,23 @@ function App() {
 
       <input
         type="text"
+        value={command}
+        onChange={(e) => setCommand(e.target.value)}
         placeholder="Bridge funds from... to..."
         className="w-[60ch] rounded-xl border-[1px] border-gray-600 p-4"
       />
+
+      <button
+        onClick={() =>
+          handleExecute({
+            command,
+            chainId: chain?.id.toString() || "",
+            signerAddress: address || zeroAddress,
+          })
+        }
+      >
+        Execute
+      </button>
     </div>
   );
 }
